d638e7bbb1048ee10cb0f8c691de0972046075d8
jeremygholland <jeremygholland@gmail.com>
2016-11-17 12:44:58 -0600

    added inital adding function

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,18 @@
-console.log('This is the Main.js file. It should be the 3rd and final file');
+var app = angular.module('app', []);
+
+
+app.controller('myCtrl' , ['$scope', function($scope){
+  $scope.movies = [];
+ $scope.submit = function(){
+   console.log('this worked?')
+   $scope.movies.push({
+     title:$scope.movie.title,
+     genre: $scope.movie.genre,
+     actors: $scope.movie.actors,
+     year: $scope.movie.year,
+     rating: $scope.movie.rating
+   })
+   return $scope.movies
+ }
+  $scope.test = "test"
+}])
