c7c69166ef38af26dd37ea6d5e2300565921b30e
jeremygholland <jeremygholland@gmail.com>
2016-11-19 11:06:11 -0600

    ran code through beautifier

--- app/js/controllers/add.js
+++ app/js/controllers/add.js
@@ -1,34 +1,33 @@
-
-app.controller('addCtrl' ,[ '$scope', '$filter','$firebase', '$state', function($scope, $filter, $firebase, $state){
-  $scope.actorAll = []
-  var clearForm = function(){
-    $scope.movie.title = '';
-    $scope.movie.genre = '';
-    $scope.movie.actors = '';
-    $scope.movie.year = '';
-    $scope.movie.rating = '';
+app.controller('addCtrl', ['$scope', '$filter', '$firebase', '$state', function($scope, $filter, $firebase, $state) {
     $scope.actorAll = []
-  }
-  var fireRef = new Firebase('https://inmotioncrud.firebaseio.com/movies');
-  $scope.movies = $firebase(fireRef).$asArray();
-  $scope.addActors = function(){
-      var actors = $scope.movie.actors;
-    $scope.actorAll.push(actors);
-    $scope.movie.actors ='';
-  }
+    var clearForm = function() {
+        $scope.movie.title = '';
+        $scope.movie.genre = '';
+        $scope.movie.actors = '';
+        $scope.movie.year = '';
+        $scope.movie.rating = '';
+        $scope.actorAll = []
+    }
+    var fireRef = new Firebase('https://inmotioncrud.firebaseio.com/movies');
+    $scope.movies = $firebase(fireRef).$asArray();
+    $scope.addActors = function() {
+        var actors = $scope.movie.actors;
+        $scope.actorAll.push(actors);
+        $scope.movie.actors = '';
+    }
 
 
 
- $scope.submitMovie = function(){
-   $scope.movie.actors = " ";
-   $scope.movies.$add({
-     title:$scope.movie.title,
-     genre: $scope.movie.genre,
-     actors: $scope.actorAll,
-     year: $scope.movie.year,
-     rating: $scope.movie.rating,
-   })
-   clearForm();
- $state.go('home');
- }
- }])
+    $scope.submitMovie = function() {
+        $scope.movie.actors = " ";
+        $scope.movies.$add({
+            title: $scope.movie.title,
+            genre: $scope.movie.genre,
+            actors: $scope.actorAll,
+            year: $scope.movie.year,
+            rating: $scope.movie.rating,
+        })
+        clearForm();
+        $state.go('home');
+    }
+}])
